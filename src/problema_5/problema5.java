049f85fa32fb0d4743d7258fd770cffa66df867d
mymac <mymac@192.168.0.3>
2020-12-17 18:16:59 -0600

    Avance Proyecto 2 - Cambio Problema 5

--- src/problema_5/problema5.java
+++ src/problema_5/problema5.java
@@ -12,25 +12,28 @@ public class problema5 {
         Scanner muestra = new Scanner(System.in);
         
         int arreglo1[];
-        int arreglo2[];
+        int arreglo2[] = {37,18,2,90,12,32,8};
         int arreglo3[];
         
         arreglo1 = new int[7];
         arreglo2 = new int [7];
         arreglo3 = new int[14];
+       
+        arreglo1[0] = 2;
+        arreglo1[1] = 3;
+        arreglo1[2] = 8;
+        arreglo1[3] = 9;
+        arreglo1[4] = 7;
+        arreglo1[5] = 0;
+        arreglo1[6] = 1;
         
-System.out.println("Digite el primer arreglo: ");
-for (int i = 0; i<7; i++){
-System.out.println("Digite un numero: ");
-arreglo1[i] = muestra.nextInt();
-}
-
-System.out.println("Digite el segundo arreglo: ");
-for (int i = 0; i<7; i++){
-System.out.println("Digite un numero: ");
-arreglo2[i] = muestra.nextInt();
-}
-
+        arreglo2[0] = 21;
+        arreglo2[1] = 33;
+        arreglo2[2] = 7;
+        arreglo2[3] = 90;
+        arreglo2[4] = 78;
+        arreglo2[5] = 10;
+        arreglo2[6] = 100;
 int j = 0;
 for (int i=0; i<7; i++){
 arreglo3[j] = arreglo1[i];
